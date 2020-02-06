767bbe28746235925e6a046c8d5252333c8fe4fe
Tristan Knight <tristanknight217@gmail.com>
2020-02-06 17:47:17 +0100

    Added first post and changed blog name

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "Tristan Knight"  # (translatable)
-BLOG_TITLE = "Tristan's Site"  # (translatable)
+BLOG_TITLE = "Tristan Knight's Blog"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://tristanknight.github.io/"
